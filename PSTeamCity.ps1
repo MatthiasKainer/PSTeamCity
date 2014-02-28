05d4b9fe79f3c4243eec4e20ff95e8091c1236f7
manathan-software-GmbH <developer@manathan.net>
2014-02-28 17:50:07 +0100

    first commit

---
+++ PSTeamCity.ps1
@@ -0,0 +1,51 @@
+$ErrorActionPreference = "Stop";
+
+register PSUrl;
+register PSColorWriter;
+
+$serverUrl = Read-Prompt-Color -Green "PSTeamCity" -White "Please insert the server url for teamcity";
+
+Set-UrlCredentials;
+
+function Get-TeamCityProjectStatus {
+	param(
+		[parameter(Mandatory = $true)][string]$project,
+		[Func[Object, bool]]$where = { param($item) -not($item.success); }
+	);
+	
+	[xml]$xml = Get-UrlContent "$serverUrl/httpAuth/app/rest/projects/$project";
+	write-output-color -Green "PSTeamcity" -White "Project $($xml.project.name) loaded"
+	$buildTypes = $xml.project.buildTypes;
+	
+	$allBuilds = @();
+	foreach ($build in $buildTypes.GetEnumerator()) {
+	    write-output-color -Green "PSTeamcity" -White "Build with id $($build.id)"
+		$allBuilds += @{ "item" = Get-TeamCityBuildStatus -buildTypeId $build.id; };
+	}
+	
+	$allBuilds | Where-Object { $where.Invoke($_.item) } | Format-Table;
+}
+
+function Get-TeamCityBuildStatus
+{
+	param(
+		[string] $buildTypeId
+	);
+	
+   	[xml]$xml = Get-UrlContent $("$serverUrl/httpAuth/app/rest/buildTypes/id:$buildTypeId/builds/running:any");
+	write-output-color -Green "PSTeamcity" -White "Build with status $($xml.build.status) and state $($xml.build.state)"
+	return @{ 
+		"success" = $xml.build.status -eq "SUCCESS";
+		"running" = $xml.build.state -eq "running";
+		"message" = "$($xml.build.buildType.name)";
+	};
+}
+
+function Set-TeamCityUrl {
+	param(
+		[parameter(Mandatory = $true)]
+		[string]$url
+	);
+	
+	$script:serverUrl = $url;
+}
